e6c2e383c0415336c9ded69fd0dcf5cd28e511ac
mahesh <gangurdemahesh9@gmail.com>
2022-11-17 18:44:28 +0530

    [mahesh]add all file for UC2

--- Day9_Practice_AddressBook/AddressBook.cs
+++ Day9_Practice_AddressBook/AddressBook.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace Day9_Practice_AddressBook
 {
-    internal class AddressBook
+    public class AddressBook
     {
+        private List<Contact> ContactList;
+        public AddressBook()
+        {
+            this.ContactList = new List<Contact>();
+        }
+
+        public void addContact(string firstName, string lastName, string address, string city, string state, string zip, string phoneNumber, string email)
+        {
+            Contact newContact = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
+            this.ContactList.Add(newContact);
+
+        }
     }
 }
--- Day9_Practice_AddressBook/Program.cs
+++ Day9_Practice_AddressBook/Program.cs
@@ -5,7 +5,26 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Day 9 Practice problem With AddressBook system");
+            AddressBook addressBook = new AddressBook();
 
+            Console.WriteLine("--Add Contact Details--");
+            Console.WriteLine("First Name:");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Last Name:");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Address:");
+            string address = Console.ReadLine();
+            Console.WriteLine("City:");
+            string city = Console.ReadLine();
+            Console.WriteLine("State:");
+            string state = Console.ReadLine();
+            Console.WriteLine("Zip Code:");
+            string zip = Console.ReadLine();
+            Console.WriteLine("Phone Number:");
+            string phoneNumber = Console.ReadLine();
+            Console.WriteLine("Email Address:");
+            string email = Console.ReadLine();
+            addressBook.addContact(firstName, lastName, address, city, state, zip, phoneNumber, email);
         }
     }
 }
