76f603afdc71a26cbff8c3c8091de337385b67cd
mahesh <gangurdemahesh9@gmail.com>
2022-11-17 18:42:02 +0530

    [mahesh]add contact file for UC1

---
+++ Day9_Practice_AddressBook/Contact.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day9_Practice_AddressBook
+{
+    internal class Contact
+    {
+    }
+}
